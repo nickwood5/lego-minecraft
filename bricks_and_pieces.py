f5f29398bc34f86ca571a9284998587a943ae5ec
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-09-04 18:50:41 -0400

    Remove unused libraries and files

--- bricks_and_pieces.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
-browser = webdriver.Chrome()
-browser.get('https://automatetheboringstuff.com')
